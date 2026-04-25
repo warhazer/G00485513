244c6ec73954caf44b8fc10fc0d75966ec8d9eac
Josh Hayes <joshuahayes06@gmail.com>
2026-04-25 22:11:07 +0100

    Add environment configuration for TMDB API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  tmdbApiKey: 'ae7194589e212cd29c5c9786ed7bbb57',
+  tmdbBaseUrl: 'https://api.themoviedb.org/3',
+  tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/w500'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  tmdbApiKey: 'ae7194589e212cd29c5c9786ed7bbb57',
+  tmdbBaseUrl: 'https://api.themoviedb.org/3',
+  tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/w500'
 };
 
 /*
