c40d28a987a22a3a8329478c0f2a6e334fbdec82
Josh Hayes <joshuahayes06@gmail.com>
2026-04-25 20:12:54 +0100

    initialize Ionic Angular standalone project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'G00485513',
+  webDir: 'www'
+};
+
+export default config;
