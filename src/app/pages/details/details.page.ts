6f47ded3a9d366d5b67015e8fe58d0c40b98cb8f
Josh Hayes <joshuahayes06@gmail.com>
2026-04-26 18:32:46 +0100

    Removed duplicate home page folder, implemented Details Page for cast and crew members with movie credits

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -1,20 +1,89 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Router, ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs'; // Import forkJoin to handle multiple API calls
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonButtons,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonList,
+  IonThumbnail,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { heart, home } from 'ionicons/icons';
+
+import { Tmdb } from '../../services/tmdb';
+import { PersonDetails, PersonMovieCredit } from 'src/app/models/movie.model';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.page.html',
   styleUrls: ['./details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonButtons,
+    IonButton,
+    IonIcon,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonList,
+    IonThumbnail,
+  ],
 })
 export class DetailsPage implements OnInit {
+  person: PersonDetails | null = null;
+  movies: PersonMovieCredit[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private tmdb: Tmdb
+  ) {
+    addIcons({ heart, home });
   }
 
+  ngOnInit() {
+    const personId = Number(this.route.snapshot.paramMap.get('id'));
+
+    // Use forkJoin to make both API calls in parallel
+    forkJoin({
+      person: this.tmdb.getPersonDetails(personId),
+      credits: this.tmdb.getPersonMovieCredits(personId),
+    }).subscribe(({ person, credits }) => {
+      this.person = person;
+      // Combine cast and crew credits into a single list of movies
+      this.movies = [...credits.cast, ...credits.crew]; 
+    });
+  }
+
+  openMovie(movie: PersonMovieCredit) { // Navigate to the movie details page
+    this.router.navigate(['/movie-details', movie.id], { state: { movie } });
+  }
+
+  goToFavourites() {
+    this.router.navigate(['/favourites']);
+  }
+
+  goHome() {
+    this.router.navigate(['/home']);
+  }
+
+  getProfileUrl(path: string | null): string {
+    return this.tmdb.getImageUrl(path);
+  }
 }
--- src/app/pages/home/home.page.spec.ts
+++ src/app/pages/home/home.page.spec.ts
@@ -1,11 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { HomePage } from './home.page';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
